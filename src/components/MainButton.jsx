0cc7e024a663e1433e5e151f580559441756a3b4
Michell Sosa <michellsosa89@gmail.com>
2021-05-26 16:39:27 -0600

    Creation and style of Button.jsx

---
+++ src/components/MainButton.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./style/MainButton.css"
+
+const Button = props =>{
+ return(
+    <React.Fragment>
+    <a className="Button" href="">
+    <p>{props.contentText}</p>
+    </a>
+    </React.Fragment>
+ );
+}
